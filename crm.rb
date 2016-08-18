193cfce1933c0dc53ff47a8aad99aa441f4ff6ca
titli1981 <madhumishra084@gmailcom>
2016-08-17 23:22:47 -0400

    implemented view all contacts

--- crm.rb
+++ crm.rb
@@ -1,8 +1,16 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative 'contact'
 require 'sinatra'
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
   erb :index
+end
+get '/contacts' do
+  Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+  Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+  Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+  
+  erb :contacts
 end
