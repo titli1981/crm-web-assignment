c68ecda57d5de8cee35ea3c4692b71bda7184d22
titli1981 <madhumishra084@gmailcom>
2016-08-18 15:31:57 -0400

    used POST and the params hash

--- crm.rb
+++ crm.rb
@@ -8,13 +8,17 @@ get '/' do
   erb :index
 end
 get '/contacts' do
-  Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-  Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-  Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+  # Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+  # Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+  # Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
   erb :contacts
 end
 
 get '/contacts/new' do
   erb :new_contact
+end
+post '/contacts'do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
