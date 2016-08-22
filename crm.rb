a7d21f5ef3b11b95a3f8d40d94711f11015729ba
titli1981 <madhumishra084@gmailcom>
2016-08-22 16:36:48 -0400

    created contacts

--- crm.rb
+++ crm.rb
@@ -25,11 +25,15 @@ end
 get '/contacts/new' do
   erb :new_contact
 end
-post '/contacts'do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+post '/contacts' do
+  contact = Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
   redirect to('/contacts')
 end
-
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)# params is a hash
   if @contact
@@ -70,4 +74,8 @@ delete '/contacts/:id' do
   else
     raise Sinatra::NotFound
   end
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
