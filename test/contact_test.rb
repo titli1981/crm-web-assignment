984e7847f33992ccd0bfcf9452e292ee822f618b
titli1981 <madhumishra084@gmailcom>
2016-08-22 17:08:25 -0400

    Updated setup method: test_update method: test_find_by method:

--- test/contact_test.rb
+++ test/contact_test.rb
@@ -2,10 +2,17 @@ require 'minitest/autorun'
 require_relative '../contact'
 
 class TestContact < Minitest::Test
-
   def setup
-    @contact = Contact.create('Grace', 'Hopper', 'grace@hopper.com', 'computer scientist')
-  end
+  @contact = Contact.create(
+    first_name: 'Grace',
+    last_name:  'Hopper',
+    email:      'grace@hopper.com',
+    note:       'computer scientist')
+end
+
+  # def setup
+  #   @contact = Contact.create('Grace', 'Hopper', 'grace@hopper.com', 'computer scientist')
+  # end
 
   def teardown
     Contact.delete_all
@@ -18,10 +25,13 @@ class TestContact < Minitest::Test
   def test_find
     assert_equal @contact, Contact.find(@contact.id)
   end
-
   def test_find_by
-    assert_equal @contact, Contact.find_by('first_name', 'Grace')
-  end
+  assert_equal @contact, Contact.find_by(first_name: 'Grace')
+end
+
+  # def test_find_by
+  #   assert_equal @contact, Contact.find_by('first_name', 'Grace')
+  # end
 
   def test_delete_all
     Contact.delete_all
@@ -31,11 +41,15 @@ class TestContact < Minitest::Test
   def test_full_name
     assert_equal 'Grace Hopper', @contact.full_name
   end
-
   def test_update
-    @contact.update('note', 'wrote the first compiler in 1952')
-    assert_equal 'wrote the first compiler in 1952', @contact.note
-  end
+  @contact.update(note: 'wrote the first compiler in 1952')
+  assert_equal 'wrote the first compiler in 1952', @contact.note
+end
+
+  # def test_update
+  #   @contact.update('note', 'wrote the first compiler in 1952')
+  #   assert_equal 'wrote the first compiler in 1952', @contact.note
+  # end
 
   def test_delete
     @contact.delete
