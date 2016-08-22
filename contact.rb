2f21a460c7a9a9f36000926a53e61e5957988c1c
titli1981 <madhumishra084@gmailcom>
2016-08-22 16:35:17 -0400

    Transforming the Contact class into an ActiveRecord Model

--- contact.rb
+++ contact.rb
@@ -7,106 +7,20 @@
 gem 'activerecord', '=4.2.7'
 require 'active_record'
 require 'mini_record'
-
+#we do not have to define a field for :id anymore. ActiveRecord will take care of creating an :id field that automatically increments!
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
-class Contact
-  attr_reader :id #getter/reader
-  attr_accessor :first_name, :last_name, :email, :note #getter/setter
-  @@contacts = []
-  @@id = 1
-  # This method should initialize the contact's attributes
-  def initialize(first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
-    @email     = email
-    @note      = note
-    @id        = @@id
-    @@id      += 1
-  end
-
-  # def email                            #getter/reader
-  #   @email
-  # end
-  # def email=(email)                     #setter/writer
-  #   @email = email
-  # end
-  # def first_name                          #getter
-  #   @first_name
-  # end
-  # def first_name=(first_name)             #getter
-  #   @first_name = first_name
-  # end
-  def self.create(first_name, last_name, email, note)
-    new_contact = Contact.new(first_name, last_name, email, note)
-    @@contacts << new_contact
-    return new_contact
-  end
-
-  # This method should return all of the existing contacts
-  def self.all
-   @@contacts                                         # OR @@contacts.each do |x|
-  end                                                #puts "#{x.first_name} #{x.last_name}"
-                                                  #puts "#{x.email}"
-                                                     #puts "#{x.note}"
-                                                     #puts "#{x.id}"
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find(ids)
-     @@contacts.find { |contact| contact.id == ids}
-  end
-
-  # This method should allow you to specify
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update(attribute,value)  #option_selected = attribute(first name, last name, name ,email)
-   case attribute
-   when "first_name" then self.first_name = value
-   when "last_name" then self.last_name  = value
-   when "email" then self.email      = value
-   when "note" then self.note       = value
-   end
-
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by(attribute,value)  # attribute = first name   value = "madhu"
-    @@contacts.each do |contact|
-      if contact.send(attribute) == value
-        return contact
-      end
-    end
-  end
-
-
-  # This method should delete all of the contacts
-  def self.delete_all
-    @@contacts.clear
-  end
+class Contact < ActiveRecord::Base
+  field :first_name, as: :string # : (data name) string(data type)
+  field :last_name,  as: :string
+  field :email,      as: :string
+  field :note,       as: :text
 
   def full_name
        "#{first_name} #{last_name}"
   end
 
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-     @@contacts.delete_if { |contact| contact.id == self.id }
-      #@@contacts.delete(self)
-  end
-
-  # Feel free to add other methods here, if you need them.
-
 end
-  # Contact.create("Madhu","Mishra","madhu.mishra20@gmail.com", "awesome")
-  # Contact.create("Arun", "Tiwari", "arun20@gmail.com", "cool")
-  # Contact.create("Harsh", "Lamba", "harsh.lamba@gmail.com","hello")
-  # Contact.all
-  # contact = Contact.find(2)
-  # puts contact.update(1,"Madhu")
-  # puts contact.first_name
-  # #puts Contact.find_by("first name","Madhu").first_name
+Contact.auto_upgrade!
+#Once the database fields are defined, we must add the following statement Contact.auto_upgrade! after the end of the class definition.
+#This one takes care of effecting any changes to the underlying structure of the tables or columns.
